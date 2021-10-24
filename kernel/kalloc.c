f03a127d5df1e921a9074190a82d012de3aa152d
hanzizhao01 <hanzizhao01@baidu.com>
2021-10-24 16:40:23 +0800

    cow lab done

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -13,6 +13,7 @@ void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
+static int count[(PHYSTOP - KERNBASE) / PGSIZE];
 
 struct run {
   struct run *next;
@@ -23,10 +24,18 @@ struct {
   struct run *freelist;
 } kmem;
 
+void incre_count(uint64 pa){
+  uint64 index = (PGROUNDUP(pa) - PGROUNDUP((uint64)end)) / PGSIZE;
+  acquire(&kmem.lock);
+  ++count[index];
+  release(&kmem.lock);
+}
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  memset(count, 0, sizeof(count));
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +44,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    incre_count((uint64)p);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,6 +62,9 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  uint64 index = ((uint64)pa-PGROUNDUP((uint64)end)) / PGSIZE;
+  if(--count[index] != 0)
+    return;
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -72,11 +86,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
+    uint64 index = (PGROUNDUP((uint64)r)-PGROUNDUP((uint64)end)) / PGSIZE;
+    count[index] = 1;
     kmem.freelist = r->next;
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
--- kernel/vm.c
+++ kernel/vm.c
@@ -302,8 +302,6 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
   pte_t *pte;
   uint64 pa, i;
-  uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -311,20 +309,17 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
+    if(*pte & PTE_W){
+      *pte = ((*pte) & (~PTE_W)) | PTE_C;
+    }
+    incre_count(pa);
+    if(mappages(new, i, PGSIZE, pa, PTE_FLAGS(*pte)) != 0){
+      kfree((void*)pa);
+      return -1;
     }
   }
   return 0;
 
- err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
-  return -1;
 }
 
 // mark a PTE invalid for user access.
@@ -346,10 +341,39 @@ uvmclear(pagetable_t pagetable, uint64 va)
 int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
+  if(dstva>MAXVA){
+    return -1;
+  }
   uint64 n, va0, pa0;
-
+  pte_t *pte;
+  char* mem;
+  uint flags;
   while(len > 0){
-    va0 = PGROUNDDOWN(dstva);
+    va0 = PGROUNDDOWN(dstva);    
+    pte = walk(pagetable, va0, 0);
+    if(pte == 0)
+      return -1;
+    if((*pte & PTE_V) == 0)
+      return -1;
+    if((*pte & PTE_U) == 0)
+      return -1;
+    if(*pte & PTE_C){
+      if((mem = kalloc()) == 0){
+        panic("copyout : can't kalloc");
+      }else{
+        pa0 = PTE2PA(*pte);
+        *pte = ((*pte) | PTE_W) & (~PTE_C);
+        flags = PTE_FLAGS(*pte);
+        memmove(mem, (char*)pa0, PGSIZE);
+      	uvmunmap(pagetable, va0, 1, 1);
+	      if(mappages(pagetable, va0, PGSIZE, (uint64)mem, flags) != 0){
+          kfree(mem);
+          panic("copyout : can't map page");
+        }
+      }
+    }else{
+      pa0 = PTE2PA(*pte);
+    }
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
